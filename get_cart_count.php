bbc05b9c7fceaeefcbd5a078adc0b28bffbb5030
maher-ashour55 <maherashoor48@gmail.com>
2025-06-29 14:03:45 +0300

    done

--- get_cart_count.php
+++ get_cart_count.php
@@ -7,11 +7,12 @@ if (!isset($_SESSION['user_id'])) {
     exit;
 }
 
-$conn = new mysqli("localhost", "user", "pass", "db");
+$conn = new mysqli("localhost", "u251541401_maher_user", "Datacoming12345", "u251541401_datacoming");
 if ($conn->connect_error) {
     echo json_encode(["success" => false, "cart_count" => 0]);
     exit;
 }
+$conn->set_charset("utf8");
 
 $user_id = $_SESSION['user_id'];
 
@@ -25,3 +26,7 @@ echo json_encode([
     "success" => true,
     "cart_count" => $row['total'] ?? 0
 ]);
+
+$stmt->close();
+$conn->close();
+?>
